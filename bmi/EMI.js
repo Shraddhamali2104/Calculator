b4a0174c264c2e71d8c3c3610f011a64e89e4129
Kushal Pathave <kushal.22210583@viit.ac.in>
2023-11-27 23:04:10 +0530

    kushal added something

--- bmi/EMI.js
+++ bmi/EMI.js
@@ -2,25 +2,22 @@ function Calculate() {
   
     // Extracting value in the amount  
     // section in the variable 
-    let amount = document.querySelector("#amount").value; 
+    const amount = document.querySelector("#amount").value; 
   
     // Extracting value in the interest 
     // rate section in the variable 
-    let rate = document.querySelector("#rate").value; 
+    const rate = document.querySelector("#rate").value; 
   
     // Extracting value in the months  
     // section in the variable 
-    let months = document.querySelector("#months").value; 
+    const months = document.querySelector("#months").value; 
   
     // Calculating interest per month 
-    let interest = (amount * (rate * 0.01)) / months; 
+    const interest = (amount * (rate * 0.01)) / months; 
       
     // Calculating total payment 
-    let total = ((amount / months) + interest).toFixed(2); 
+    const total = ((amount / months) + interest).toFixed(2); 
   
-<<<<<<< HEAD
-    
-=======
->>>>>>> 8b8f73b39fe8a61ef75473f9f97129c86e0ca073
-    document.querySelector("#total").innerHTML = "EMI : (₹)" + total; 
+    document.querySelector("#total") 
+            .innerHTML = "EMI : (₹)" + total; 
 }
